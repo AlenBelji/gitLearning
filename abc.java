bb2cede1201aef66e6cdaef4dc1e13034788f9d7
Alen <alenbeljimini@gmail.com>
2024-01-03 21:57:20 +0530

    Version 1.1

---
+++ abc.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class abc{
+    public static void main(String args[]){
+        Scanner in=new Scanner(System.in);
+        String a[]=new String[10];
+        for(int i=0;i<10;i++){
+            a[i]=in.nextLine();
+        }
+        int x=in.nextInt();
+        System.out.println("Value -"+a[x]);
+    }
+}
